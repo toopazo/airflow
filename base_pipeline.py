904038b0e660815e4385dbbb8874208893d46bb2
toopazo <toopazo@proton.me>
2025-02-02 23:59:55 -0300

    se agrega codigo de chain of responsability para implementar pipeline

---
+++ base_pipeline.py
@@ -0,0 +1,131 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Any, Optional
+from copy import deepcopy
+
+# List of video databases for face recognition
+# https://github.com/polarisZhao/awesome-face/blob/master/README.md
+
+# YouTube Faces DB
+# https://www.cs.tau.ac.il//~wolf/ytfaces/
+
+# Design Pattern - Chain of Responsibility
+# https://refactoring.guru/design-patterns/chain-of-responsibility/python/example
+
+
+class NameRequestResponse:
+    """
+    Class to store the request and response entities
+    """
+
+    def __init__(self, name: str, request: dict, response: dict) -> None:
+        self.name = name
+        self.request = request
+        self.response = response
+
+        request_log_key = self.request.get(BaseHandler.log_key)
+        if isinstance(request_log_key, str):
+            self.request_str = request_log_key
+        else:
+            self.request_str = f"Request has no [{BaseHandler.log_key}] key"
+
+        response_log_key = self.response.get(BaseHandler.log_key)
+        if isinstance(response_log_key, str):
+            self.response_str = response_log_key
+        else:
+            self.response_str = f"Response has no [{BaseHandler.log_key}] key"
+
+    def __str__(self) -> str:
+        n_cols = 10
+        m1 = self.name.ljust(int(n_cols * 2))
+
+        m2a = ", ".join(self.request.keys())
+        m2a = f"[{m2a}]".ljust(int(n_cols))
+        m2b = self.request_str.ljust(int(n_cols * 4))
+
+        m3a = ", ".join(self.response.keys())
+        m3a = f"[{m3a}]".ljust(int(n_cols))
+        m3b = self.response_str.ljust(int(n_cols * 4))
+
+        msg = f"name: {m1} | request keys: {m2a} request[{BaseHandler.log_key}]: {m2b} | response keys: {m3a} response[{BaseHandler.log_key}]: {m3b}"
+        return msg
+
+
+class Handler(ABC):
+    """
+    The Handler interface declares a method for building the chain of handlers.
+    It also declares a method for executing a request.
+    """
+
+    log_key = "to_str"
+    early_stop_key = "early_stop"
+
+    @abstractmethod
+    def set_next(self, handler: Handler) -> Handler:
+        """
+        Method for building the chain of handlers.
+        """
+
+    @abstractmethod
+    def handle(self, request) -> NameRequestResponse:
+        """
+        Method for executing the request
+        """
+
+    @abstractmethod
+    def get_response(self, request) -> dict:
+        """
+        Method for processing the request and getting a response
+        """
+
+    @abstractmethod
+    def early_stop_response(self) -> dict:
+        """
+        Method for processing the response upon an early stop
+        """
+
+
+class BaseHandler(Handler):
+    """
+    The default chaining behavior can be implemented inside a base handler
+    class.
+    """
+
+    def __init__(self) -> None:
+        self.__next_handler: Optional[Handler] = None
+
+    def set_next(self, handler: Handler) -> Handler:
+        self.__next_handler = handler
+        # Returning a handler from here will let us link handlers in a
+        # convenient way like this:
+        # monkey.set_next(squirrel).set_next(dog)
+        return handler
+
+    def handle(self, request: dict) -> NameRequestResponse:
+        response = self.get_response(request)
+        nrr = NameRequestResponse(
+            name=self.__class__.__name__, request=request, response=response
+        )
+        print(nrr)
+        if response.get(Handler.early_stop_key):
+            return nrr
+        else:
+            if hasattr(self.__next_handler, "handle"):
+                # return self.__next_handler.handle(request=nrr.response)
+                next_handle = getattr(self.__next_handler, "handle")
+                return next_handle(nrr.response)
+            else:
+                return nrr
+
+    def get_response(self, request: dict) -> dict:
+        # Add info, then pass it on to the next handler
+        # return request
+        response = deepcopy(request)
+        response[Handler.log_key] = f"Default log from {self.__class__.__name__}"
+        return response
+
+    def early_stop_response(self) -> dict:
+        return {
+            BaseHandler.log_key: "Early stop was triggered",
+            BaseHandler.early_stop_key: True,
+        }
---
+++ main.py
@@ -0,0 +1,78 @@
+from base_pipeline import BaseHandler, Handler
+
+# List of video databases for face recognition
+# https://github.com/polarisZhao/awesome-face/blob/master/README.md
+
+# YouTube Faces DB
+# https://www.cs.tau.ac.il//~wolf/ytfaces/
+
+# Design Pattern - Chain of Responsibility
+# https://refactoring.guru/design-patterns/chain-of-responsibility/python/example
+
+
+class MonkeyHandler(BaseHandler):
+    def get_response(self, request: dict) -> dict:
+        if request[BaseHandler.log_key] == "Banana":
+            response = f"{self.__class__.__name__}: I'll eat the {request[BaseHandler.log_key]}"
+            return {BaseHandler.log_key: response}
+        else:
+            # return super().get_response(request)
+            return super().early_stop_response()
+
+
+class SquirrelHandler(BaseHandler):
+    def get_response(self, request: dict) -> dict:
+        if request[BaseHandler.log_key] == "Nut":
+            response = f"{self.__class__.__name__}: I'll eat the {request[BaseHandler.log_key]}"
+            return {BaseHandler.log_key: response}
+        else:
+            # return super().get_response(request)
+            return super().early_stop_response()
+
+
+class DogHandler(BaseHandler):
+    def get_response(self, request: dict) -> dict:
+        if request[BaseHandler.log_key] == "MeatBall":
+            response = f"{self.__class__.__name__}: I'll eat the {request[BaseHandler.log_key]}"
+            return {BaseHandler.log_key: response}
+        else:
+            # return super().get_response(request)
+            return super().early_stop_response()
+
+
+def client_code(handler: Handler) -> None:
+    """
+    The client code is usually suited to work with a single handler. In most
+    cases, it is not even aware that the handler is part of a chain.
+    """
+
+    for log_key_request in ["Nut", "Banana", "Cup of coffee"]:
+        request = {BaseHandler.log_key: log_key_request}
+        print(
+            f"\nClient input request[{BaseHandler.log_key}]: {request[BaseHandler.log_key]}?"
+        )
+        nrr = handler.handle(request)
+        print(
+            f"\nClient output response[{BaseHandler.log_key}]: {nrr.response[BaseHandler.log_key]}?"
+        )
+        # if result:
+        #     print(f"  {result}", end="")
+        # else:
+        #     print(f"  {food} was left untouched.", end="")
+
+
+if __name__ == "__main__":
+    monkey = MonkeyHandler()
+    squirrel = SquirrelHandler()
+    dog = DogHandler()
+
+    monkey.set_next(squirrel).set_next(dog)
+
+    # The client should be able to send a request to any handler, not just the
+    # first one in the chain.
+    print("Chain: Monkey > Squirrel > Dog")
+    client_code(monkey)
+    print("\n")
+
+    # print("Subchain: Squirrel > Dog")
+    # client_code(squirrel)
